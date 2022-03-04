67bccb0574dd936172b56cdad919e6b58fb222e0
David Zomada <david.zomada@gmail.com>
2022-03-04 13:28:31 +0100

    Add property picture to Plant

--- APIDemo/Models/Plant.cs
+++ APIDemo/Models/Plant.cs
@@ -5,8 +5,16 @@ namespace APIDemo.Models {
     public int PlantId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Picture { get; set; }
 
     public Plant() {
+    }
+
+    public Plant(int PlantId, string Name, string Description, string Picture) {
+      this.PlantId = PlantId;
+      this.Name = Name;
+      this.Description = Description;
+      this.Picture = Picture;
     }
   }
 }
